366a89ffda4a22a32dbb7ae6ad5eb4554d8482c6
Rahul <panthamr14@gmail.com>
2025-11-19 16:06:21 +0530

    new changes

--- src/main/java/se/mavenjava/App.java
+++ src/main/java/se/mavenjava/App.java
@@ -5,6 +5,6 @@ package se.mavenjava;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! how are you");
+        System.out.println("Hello World! how are you world");
     }
 }
