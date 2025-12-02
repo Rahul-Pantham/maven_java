ab1555a15d80a69aa0273eea4186e45b81c3a73a
Rahul <panthamr14@gmail.com>
2025-12-02 09:43:59 +0530

    newchange

--- src/main/java/se/mavenjava/App.java
+++ src/main/java/se/mavenjava/App.java
@@ -5,6 +5,6 @@ package se.mavenjava;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! how are you world"123456789);
+        System.out.println("Hello World! how are you world147852369123456789");
     }
 }
